edeed27be8fd4b0e8fcc43e0aeb581609aabaefc
abbygraves <abbygraves14@gmail.com>
2022-06-11 22:28:17 -0500

    5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ var dailyWeatherEl = document.querySelector("#daily-weather");
 var cityArray = [];
 var cityHistoryEl = document.querySelector("#city-history");
 
+
 function formSubmitHandler(event) {
   event.preventDefault();
   var city = cityInput.value.trim();
@@ -24,17 +25,20 @@ function formSubmitHandler(event) {
     saveCityHistory(city);
     getWeather(city);
     cityInput.value = "";
+    dailyWeatherEl.textContent = "";
   } else {
     alert("Please enter a city");
   }
 };
 
+
 function saveCityHistory(city) {
   cityArray.push(city);
   console.log(cityArray)
   localStorage.setItem("cityArray", JSON.stringify(cityArray));
- 
+  
 };
+
 
 function loadCityHistory() {
   var saveCityHistory = JSON.parse(localStorage.getItem("cityArray"));
@@ -44,8 +48,8 @@ function loadCityHistory() {
     for (let i = 0; cityArray.length; i++) {
       var historyButtonEl = document.createElement("button");
       historyButtonEl.textContent = cityArray[i];
+      historyButtonEl.classList = "btn-search";
       cityHistoryEl.appendChild(historyButtonEl);
-      
     }
   }
 };
@@ -65,10 +69,11 @@ function getWeather(location) {
   })
 };
 
+
 function getLatLong(data) {
   var locationLat = data.coord.lat;
   var locationLon = data.coord.lon;
-  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + locationLat + "&lon=" + locationLon + "&appid=f3abb8e7ac5dca95fb34c9719d493299";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + locationLat + "&lon=" + locationLon + "&appid=f3abb8e7ac5dca95fb34c9719d493299&units=imperial";
 
   fetch(apiUrl).then(function (response) {
     // request was succesful
@@ -83,27 +88,6 @@ function getLatLong(data) {
   })
 };
 
-function displayDailyWeather(dailyWeather, timezone) {
-  console.log(dayjs().tz(timezone).add(1, "day").startOf("day").format("M/D/YYYY"))
-  for (let i = 0; i < 5; i++) {
-    console.log(dailyWeather[i])
-
-    // make the elements
-    var dateEl = document.createElement("h5");
-    var humidityDailyEl = document.createElement("p");
-    var rightNow1 = dayjs().tz(timezone).add(i, "day").startOf("day").format("M/D/YYYY");
-
-    // assigning values to elements
-    dateEl.textContent = rightNow1;
-    humidityDailyEl.textContent = dailyWeather[i].humidity;
-
-    // append them all
-    dailyWeatherEl.appendChild(dateEl);
-    dailyWeatherEl.appendChild(humidityDailyEl);
-    
-  }
-};
-
 
 function displayWeather(data) {
   cityNameEl.textContent = data.name;
@@ -113,15 +97,15 @@ function displayWeather(data) {
 
   var timezone = data.timezon
   
-  var rightNow = dayjs().tz(timezone).add(1, "day").startOf("day").format("M/D/YYYY");
+  var rightNow = dayjs().tz(timezone).add(0, "day").startOf("day").format("M/D/YYYY");
   currentDateEl.textContent = rightNow;
   // (for i = 0 )
 
   var iconCode = data.weather[0].icon;
   var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
   $('#wicon').attr('src', iconUrl);
-  
 };
+
 
 
 function displayUV(data) {
@@ -142,8 +126,42 @@ function displayUV(data) {
 };
 
 
+
+function displayDailyWeather(dailyWeather, timezone) {
+  // console.log(dayjs().tz(timezone).add(1, "day").startOf("day").format("M/D/YYYY"))
+  for (let i = 1; i < 6; i++) {
+    console.log(dailyWeather[i])
+
+    // make the elements
+    var dateDailyEl = document.createElement("h5");
+    var humidityDailyEl = document.createElement("p");
+    var rightNow1 = dayjs().tz(timezone).add(i, "day").startOf("day").format("M/D/YYYY");
+    var tempDailyEl = document.createElement("p");
+    var windDailyEl = document.createElement("p");
+
+    // assigning values to elements
+    dateDailyEl.textContent = rightNow1;
+    tempDailyEl.textContent = "Temp: " + dailyWeather[i].temp.day + "°F";
+    windDailyEl.textContent = "Wind: " + dailyWeather[i].wind_speed + " MPH"
+    humidityDailyEl.textContent = "Humidity: " + dailyWeather[i].humidity + " %";
+
+    // // add classes
+    // dateDailyEl.classList = "days";
+    // tempDailyEl.classList = "days";
+
+    // append them all
+    dailyWeatherEl.appendChild(dateDailyEl);
+    dailyWeatherEl.appendChild(tempDailyEl);
+    dailyWeatherEl.appendChild(windDailyEl);
+    dailyWeatherEl.appendChild(humidityDailyEl);
+    
+  }
+};
+
+
+cityForm.addEventListener("submit", loadCityHistory);
 cityForm.addEventListener("submit", formSubmitHandler);
-// loadCityHistory();
+
 
 
 
